c0221f8e87186d59894fa85eb6ac2f181a139326
Van Tran <van.tran@asnet.com.vn>
2025-05-25 23:35:50 +0700

    handle log api

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,5 +1,8 @@
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
+
+import logger from './logger';
 
 export const removeBodyId = (req: Request, _: Response, next: NextFunction) => {
   if (req.body) {
@@ -8,8 +11,18 @@ export const removeBodyId = (req: Request, _: Response, next: NextFunction) => {
   next();
 };
 
+const morganMiddleware = morgan('common', {
+  stream: {
+    write: (message) => {
+      logger.info(message.trim());
+    },
+  },
+});
+
 export const configureMiddlewares = (app: express.Application): void => {
   app.use(express.json());
+
+  app.use(morganMiddleware);
 
   app.use(express.urlencoded({ extended: true }));
 
